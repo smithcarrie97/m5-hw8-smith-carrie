c7305da5c0c8626591391fefc2ed803b3a2d0fb2
Carrie Smith <smithcarrierenee@gmail.com>
2021-10-16 18:08:56 -0400

    added media queries

--- unsolved/index.js
+++ unsolved/index.js
@@ -1,10 +1,28 @@
 // Create an Array of at least 3 losing messages
+var losingMessages;
+losingMessages = ['You lose!','Try again.','Wrong choice!'];
 
 // Create variables to count wins and losses
+var wins = 0;
+var losses = 0;
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
+var messageEl = document.getElementById('message');
+var winsEl = document.getElementById('wins');
+var lossesEl = document.getElementById('losses');
 
 // target all .box elements and attach a click event listener to each one using a loop
+var boxElements = document.querySelectorAll('.box')
+
+for (var i = 0; i < boxElements.length; i++) {
+    var boxElement = boxElements[i];
+    console.log(boxElement)
+    boxElement.onclick = function(event) {
+        var numWasClicked = Number(event.target.textContent)
+        console.log(parseInt(boxNumWasClicked, 10))
+        console.log(Number(boxNumWasClicked))
+    }
+}
 
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
@@ -12,10 +30,17 @@
 
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
+var winningNumber = 3;
 
 // determine if the box clicked is equal to the random number
 // if the numbers match, display a winning message by changing the text content of the div#message element
 // if the numbers match, increment wins and display the win count in div#wins
-
+if (numWasClicked = winningNumber) {
+    document.getElementById("message").textContent = "You Win!"
+    document.getElementById("wins")++
+} else {
+document.getElementById("message").textContent = losingMessages
+document.getElementById("losses")++
+}
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
